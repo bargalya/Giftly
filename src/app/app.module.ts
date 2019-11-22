5f7a5787b9ab6a9238158565de543d22fb85d045
yafitlivshitz <yafit.mo@gmail.com>
2019-11-22 23:07:56 +0200

    Initial start page & adding an empty subscribe module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SubscribeComponent } from './subscribe/subscribe.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SubscribeComponent
   ],
   imports: [
     BrowserModule,
