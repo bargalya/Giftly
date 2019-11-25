12dfd24f9728d1543b36d373c9c53273959462ea
Lika Mamia <lika@google.com>
2019-11-25 23:49:37 +0200

    add events dashboard (empty) component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { EventsDashboardComponent } from './events-dashboard/events-dashboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [{path: 'events-dashboard', component: EventsDashboardComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
