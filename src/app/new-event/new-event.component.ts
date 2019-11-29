17aea9bb0ff38e0410d0de45ee41564ee6811edb
Inna Gurevich <inna.gu@gigya-inc.com>
2019-11-29 19:53:31 +0200

    update gifts list

--- src/app/new-event/new-event.component.ts
+++ src/app/new-event/new-event.component.ts
@@ -1,5 +1,8 @@
+import { Gift, GiftStatus } from './../models/gift.class';
 import { Component, OnInit } from '@angular/core';
-
+import {AllCommunityModules} from '@ag-grid-community/all-modules';
+import { Event } from '../models/event.class';
+import { forEach } from '@angular/router/src/utils/collection';
 @Component({
   selector: 'app-new-event',
   templateUrl: './new-event.component.html',
@@ -7,9 +10,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewEventComponent implements OnInit {
 
+  columnDefs = [
+    {headerName: 'URL', field: 'giftUrl' },
+    {headerName: 'Status', field: 'status' }
+  ]
+
+  giftsList = [ ]
+
+modules = AllCommunityModules;
+
   constructor() { }
 
   ngOnInit() {
+    var gifts = new Array<Gift>();
+    for(let i=0; i<this.giftsList.length; i++){
+      gifts.push(new Gift(this.giftsList[i].giftUrl, this.giftsList[i].status)); 
+    }
+    var name = (document.getElementById("event-name") as HTMLInputElement).value;
+    var description = (document.getElementById("event-description") as HTMLInputElement).value;
+    this._event = new Event(name, description, gifts);
   }
+
+
+  addGift() : void  {
+    var giftUrl = (document.getElementById("gift-url") as HTMLInputElement).value;
+    var totalGifts = [{ giftUrl: giftUrl, status: GiftStatus.ReadyForGrabs}];
+    
+    var gifts = [];
+    if(this._event != null) 
+      gifts = this._event.getGifts();
+    for(let i=0; i<gifts.length; i++){
+      totalGifts.push({ giftUrl: gifts[i].Url, status: gifts[i].Status});
+    }
+    this.giftsList = totalGifts;
+    gifts.push(new Gift(giftUrl, GiftStatus.ReadyForGrabs));
+    this._event.setGifts(gifts);
+  }
+
+  createEvent() : void {
+    //TODO:send the event to BE and save in to the DB
+  }
+  _event:Event;
 
 }
