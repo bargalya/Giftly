20c6f77410501da1060857fec527c16f72ab4df9
bargalya <bargalya@gmail.com>
2019-12-04 19:28:38 +0200

    adding proxy json and example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'giftly';
+
+  constructor(private http: HttpClient) {
+    this.http.get('api/user/37362').subscribe(res => {console.log('BE result:', res)});
+  }
 }
