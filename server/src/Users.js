186fe54fa9007f09f9e923bd318ab671bd59891b
yafitlivshitz <yafit.mo@gmail.com>
2019-12-14 16:08:58 +0200

    Move DB handling to a separate module

--- server/src/Users.js
+++ server/src/Users.js
@@ -1,4 +1,9 @@
-const mongo = require('mongodb').MongoClient;
+//const mongo = require('mongodb').MongoClient;
+//const dbMgr = require('../dbMgr/dbMgr');
+
+const getDb = require("./db").getDb;
+const addToDb = require('../dbMgr/dbMgr').addToDb;
+
 
 class Users{
     static url = "mongodb://localhost:27017/";
@@ -15,7 +20,9 @@ class Users{
     // TODO: should we support GET method?
     // I don't see any reason to support
     get(req, res) {
-//        const mongo = require('mongodb').MongoClient; duplicated move out
+
+        //********************* old code - delete */
+/*        const mongo = require('mongodb').MongoClient; duplicated move out
         const ObjectId = require('mongodb').ObjectID;
         const userid = ObjectId(req.params.userid);
         let query = {'_id' : userid};
@@ -39,13 +46,59 @@ class Users{
                             'data': document
                             });
                 });
-        });        
+        });        */
+        
+
+        /********************************* new code - uncomment and check it works 
+        dbMgr.find(req.params.userid, Users.collectionName,
+            function(err, document)
+            {
+                if(err) {
+                    res.send({'status': 'Failed',
+                            'error': err});
+                }
+                else {
+                res.send({
+                    'status': 'success',
+                    'data': document}
+                )};
+            });        
+            */
     }
 
     add(req, res){
+
+        const userName = req.body.userName,
+        password = req.body.password;
+
+        const document = { 
+            UserName: userName,
+            Password: password               
+        };                 
+
+        // const db = getDb();
+
+        console.log("got a post request: new user request name " + userName + " password " + password);        
+        addToDb(Users.collectionName, document,
+            function(err, responseDocument) {
+                if (err)
+                {
+                    res.send({
+                        'status': 'Failed',
+                        'error': err});
+                }
+                else
+                {
+                    res.send({
+                        'status': 'success',
+                        'data': responseDocument
+                    });                
+                }
+            });
+        
     //    const mongo = require('mongodb').MongoClient; duplicated - move out
         // Read the user parameters
-        const userName = req.body.userName,
+   /*     const userName = req.body.userName,
         password = req.body.password;
 
         console.log("got a post request: new user request name " + userName + " password " + password);        
@@ -69,7 +122,7 @@ class Users{
                             res.send({'status': 'Failed',
                                     'error': err});
                         }                        
-                       
+               */        
 // Can't seem to send a response to the front end. Handle this part later
                         // Send a response to the front end
                 /*        res.send({"status":"success",
@@ -82,15 +135,29 @@ class Users{
                             'data': document
                             });
                         })}                         
-                */
+                
                     });                  
     
-                client.close();       
-        });
+             //   client.close();       
+        });*/
     }
 
     update(req, res){
-        
+
+        dbMgr.update(req.params.userid, Users.collectionName, req.body,
+            function(err, response) {
+                if(err) {
+                    res.send({'status': 'Failed',
+                            'error': err});
+                }
+                else {
+                    res.send({
+                        'status': 'success',
+                        'data': response // TODO: I am not sure what should I send back
+                        });
+                }
+            });
+        /*
         const ObjectId = require('mongodb').ObjectID;
         const userid = ObjectId(req.params.userid);
         let query = {'_id' : userid};
@@ -115,7 +182,7 @@ class Users{
                             'data': response
                             });
                 });
-        });        
+        });       */        
     }
 }
 
