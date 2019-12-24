bb7dbd2bf60740e20e12a54462cac9482a487ff5
Inna Gurevich <inna.gu@gigya-inc.com>
2019-12-24 16:15:41 +0200

    add event to the db (without gifts)

--- server/src/Events.js
+++ server/src/Events.js
@@ -1,7 +1,10 @@
+const addToDb = require('../dbMgr/dbMgr').addToDb;
+var uuidCreator = require('uuid');
+
 class Events{
 
     constructor(){
-
+        Events.collectionName = "Events"; 
     }
 
     async get(req, res){
@@ -13,9 +16,38 @@ class Events{
 
     }
 
+    // Handle event request
     add(req, res){
 
-    }
+        // Initialize the DB document
+        const document = { 
+            name: req.body.name,
+            description: req.body.description,
+            date: req.body.date,
+            euid: uuidCreator.v4()               
+        };            
+                
+        // Insert the document to the DB
+        addToDb(Events.collectionName, document,
+            function(err, responseDocument) {
+                if (err)
+                {
+                    res.send({
+                        'status': 'Failed',
+                        'error': err});
+                }
+                else
+                {
+
+                    console.log("A new event was added. event: " + document.name);
+
+                    res.send({
+                        'status': 'success',
+                        'data': responseDocument
+                    });                
+                }
+            });  
+        }
 }
 
 module.exports = Events;
