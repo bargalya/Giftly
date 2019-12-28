1504a57255926698b5d02a38b833b50d5e707779
zimgalia <zimgalia@gmail.com>
2019-12-28 14:50:01 +0200

    Add an optional title field to Gift class

--- src/app/models/gift.class.ts
+++ src/app/models/gift.class.ts
@@ -1,9 +1,13 @@
+import { stringify } from 'querystring';
+
 export class Gift {
     public Url: string;
     public Status: GiftStatus;
-    constructor(url: string, status: GiftStatus)    {
+    public Title: string;
+    constructor(url: string, status: GiftStatus, title?: string) {
         this.Url = url;
         this.Status = status;
+        this.Title = title;
     }
 }
 
