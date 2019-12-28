900b2e5d7e49ceb4614c37afa346c2c19887ccc3
Inna Gurevich <inna.gu@gigya-inc.com>
2019-12-28 14:43:47 +0200

    split to funcs

--- server/src/Events.js
+++ server/src/Events.js
@@ -1,6 +1,5 @@
 const addToDb = require('../dbMgr/dbMgr').addToDb;
 const addManyToDb = require('../dbMgr/dbMgr').addManyToDb;
-var uuidCreator = require('uuid');
 
 class Events{
 
@@ -18,62 +17,71 @@ class Events{
 
     }
 
-    add(req, res){
-        const eventId = uuidCreator.v4();
-        let gifts = JSON.parse(req.body.gifts);
-        let giftsDocument = [];
-        gifts.forEach(gift => { 
-            giftsDocument.push(
-                {
-                    url:gift["Url"],
-                    status:gift["Status"],
-                    eventId: eventId
-                }
-            ); 
-          });
-
-        let responseGiftsDocument;
-        addManyToDb(Events.giftsCollectionName, giftsDocument,
-            function(err, responseDocument) {
-                if (err)
-                {
-                    res.send({
-                        'status': 'Failed',
-                        'error': err});
-                }
-                else
-                {
-                    console.log("New gifts were added.");       
-                    responseGiftsDocument = responseDocument;
-                }
-            });  
-            
-        const document = { 
-            name: req.body.name,
-            description: req.body.description,
-            date: req.body.date,
-            euid: eventId             
-        };            
-        
-        addToDb(Events.collectionName, document,
-            function(err, responseDocument) {
-                if (err)
-                {
-                    res.send({
-                        'status': 'Failed',
-                        'error': err});
-                }
-                else
-                {
-                    console.log("A new event was added. event: " + document.name);
-                    res.send({
-                        'status': 'success',
-                        'eventData': responseDocument,
-                        'giftsdata': responseGiftsDocument
-                    });                
-                }
-            });  
-        }
+    add(req, res)    
+    {
+        let responseEventDocument = saveEvent(req.body.name, req.body.description, req.body.date, res);
+        saveGifts(req.body.gifts, responseEventDocument, res);
+    }
 }
+
+
+var saveGifts = function(giftsReq, responseEventDocument, res) {
+    let gifts = JSON.parse(giftsReq);
+    let giftsDocument = [];
+    gifts.forEach(gift => { 
+        giftsDocument.push(
+            {
+                url:gift["Url"],
+                status:gift["Status"],
+                //eventId: responseEventDocument["_id"]
+            }
+        ); 
+      });
+
+    addManyToDb(Events.giftsCollectionName, giftsDocument,
+        function(err, responseDocument) {
+            if (err)
+            {
+                res.send({
+                    'status': 'Failed',
+                    'error': err});
+            }
+            else
+            {
+                console.log("New gifts were added.");       
+                res.send({
+                    'status': 'success',
+                    'eventData': responseEventDocument,
+                    'giftsdata': responseDocument
+                }); 
+            }
+        });
+}
+var saveEvent = function(name, description, date, res) { 
+    let responseEventDocument;
+    const document = { 
+        name: name,
+        description: description,
+        date: date
+    };            
+    
+    addToDb(Events.collectionName, document,
+        function(err, responseDocument) {
+            if (err)
+            {
+                console.log("error adding an event: " + document.name);
+                res.send({
+                    'status': 'Failed',
+                    'error': err});
+            }
+            else
+            {                
+                responseEventDocument = responseDocument;  
+                console.log("A new event was added. event: " + document.name + " eventId: " + responseEventDocument["_id"]);     
+            }
+        });
+        //console.log("!!!!!A new event was added. event: " + document.name + " eventId: " + responseEventDocument["_id"]); 
+        return responseEventDocument;
+} 
 
 module.exports = Events;
