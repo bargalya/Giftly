15923b13b38d4fca769d755bb8db5e27b968f518
galyab <galya.bar@nice.com>
2020-03-01 16:04:33 +0200

    db change

--- server/src/dbMgr/DbMgr.js
+++ server/src/dbMgr/DbMgr.js
@@ -1,6 +1,7 @@
 const mongo = require('mongodb').MongoClient;
 
-url = "mongodb://localhost:27017/";    
+//url = "mongodb://localhost:27017/";
+url = "mongodb+srv://galya:VEpJqOuCnq71fP3Z@cluster0-olsrr.mongodb.net/test?retryWrites=true&w=majority";
 dbName = "giftlyDB";
 connectParams = {
         useNewUrlParser: true,
@@ -10,22 +11,22 @@ connectParams = {
 let db;
 
 // Connect to the DB and initialize the variable with the connection
-mongo.connect(url, connectParams, 
+mongo.connect(url, connectParams,
     function(err, client) {
-        if(err) {                        
+        if(err) {
             console.log("ERROR! failed to connect to Data base!");
         }
         else {
             db = client.db(dbName);
             console.log("Database created");
-        }    
+        }
     }
 );
 
 async function addToDb(collectionName, document)
 {
     if (db)
-    {       
+    {
         return await insertOne(collectionName, document);
     }
     else
@@ -33,22 +34,22 @@ async function addToDb(collectionName, document)
         // the object is expected to be initialized at the application's rise
         console.log("ERROR!!! from func add: DB is not connected! connecting now. fix bug later!");
 
-        mongo.connect(url, connectParams, 
+        mongo.connect(url, connectParams,
             async function(err, client) {
-                if(err) {                    
+                if(err) {
                     console.log("Error!");
                 }
                 db = client.db(dbName);
                 console.log("Database created!" + db);
                 return await insertOne(collectionName, document);
                 }
-        );    
-    }        
+        );
+    }
 }
 
 async function insertOne(collectionName, document) {
     try{
-        const collection = db.collection(collectionName);  
+        const collection = db.collection(collectionName);
         return await collection.insertOne(document);
     }
     catch(err)
@@ -60,10 +61,10 @@ async function insertOne(collectionName, document) {
 
 async function addManyToDb(collectionName, document)
 {
-    
+
     console.log("collectionName!!!!!!" + collectionName);
     if (db)
-    {        
+    {
         return await insertMany(collectionName, document);
     }
     else
@@ -71,24 +72,24 @@ async function addManyToDb(collectionName, document)
         // the object is expected to be initialized at the application's rise
         console.log("ERROR!!! from func add: DB is not connected! connecting now. fix bug later!");
 
-        mongo.connect(url, connectParams, 
+        mongo.connect(url, connectParams,
             async function(err, client) {
-                if(err) {                    
+                if(err) {
                     console.log("Error!");
                 }
                 db = client.db(dbName);
                 console.log("Database created!" + db);
                 return await insertMany(collectionName, document);
                 }
-        );    
-    }        
+        );
+    }
 }
 
 async function insertMany(collectionName, documents)
 {
     try {
-        const collection = db.collection(collectionName);    
-        return await collection.insertMany(documents);  
+        const collection = db.collection(collectionName);
+        return await collection.insertMany(documents);
     }
     catch(err) {
         console.log("failed to add a documents to " + collection + " collection");
@@ -97,7 +98,7 @@ async function insertMany(collectionName, documents)
 }
 
 async function findOne(quary, collectionName)
-{        
+{
     try {
         let collection = db.collection(collectionName);
         return await collection.findOne(quary);
@@ -106,11 +107,11 @@ async function findOne(quary, collectionName)
     {
         console.log("DbMgr: failed to find a document in " + collectionName + " collection");
         throw new Error("DbMgr: failed to find a document in " + collectionName + " collection");
-    }   
+    }
 }
 
 async function search(query, collectionName)
-{  
+{
     return await findOne(query, collectionName);
 }
 
@@ -118,7 +119,7 @@ async function findMany(query, collectionName) {
     if(db) {
         try {
             const collection = db.collection(collectionName);
-            return await collection.find(query).toArray();    
+            return await collection.find(query).toArray();
         }
         catch(err) {
             console.log("DbMgr: failed to find documents in " + collectionName + " collection. Error:", err);
@@ -143,8 +144,8 @@ async function update(query, newValues, collectionName) {
         console.log("DbMgr Error: No connection to DB")
     }
 }
-           
-module.exports = { 
+
+module.exports = {
     addToDb,
     addManyToDb,
     search,
